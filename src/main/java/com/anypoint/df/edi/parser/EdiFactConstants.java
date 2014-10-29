17b48459e30ea7e4e5fea7c0d484398c73f3be76
Dennis Sosnoski <dms@sosnoski.com>
2014-10-29 23:35:01 +1300

    Include new constant definition interfaces used by token parser code.

---
+++ src/main/java/com/anypoint/df/edi/parser/EdiFactConstants.java
@@ -0,0 +1,28 @@
+
+package com.anypoint.df.edi.parser;
+
+/**
+ * Constants for EDIFACT documents.
+ */
+public interface EdiFactConstants
+{
+    public static final String SYNTAX_IDENTIFIER = "Syntax identifier";
+    public static final String SYNTAX_VERSION_NUMBER = "Syntax version number";
+    public static final String SENDER_IDENTIFICATION = "Sender identification";
+    public static final String SENDER_IDENTIFICATION_CODE_QUALIFIER = "Sender partner identification code qualifier";
+    public static final String REVERSE_ROUTING_ADDRESS = "Address for reverse routing";
+    public static final String RECIPIENT_IDENTIFICATION = "Recipient identification";
+    public static final String RECIPIENT_IDENTIFICATION_CODE_QUALIFIER =
+        "Recipient partner identification code qualifier";
+    public static final String RECIPIENT_ROUTING_ADDRESS = "Onward routing address";
+    public static final String PREPARATION_DATE = "Date of preparation";
+    public static final String PREPARATION_TIME = "Time of preparation";
+    public static final String INTERCHANGE_CONTROL_REFERENCE = "Interchange control reference";
+    public static final String RECIPIENT_REFERENCE = "Recipient's reference/password";
+    public static final String RECIPIENT_REFERENCE_QUALIFIER = "Recipient's reference/password qualifier";
+    public static final String APPLICATION_REFERENCE = "Application reference";
+    public static final String PROCESSING_PRIORITY_CODE = "Processing priority code";
+    public static final String ACKNOWLEDGEMENT_REQUEST = "Acknowledgement request";
+    public static final String COMMUNICATIONS_AGREEMENT_ID = "Communications agreement id";
+    public static final String TEST_INDICATOR = "Test indicator";
+}
---
+++ src/main/java/com/anypoint/df/edi/parser/X12Constants.java
@@ -0,0 +1,50 @@
+
+package com.anypoint.df.edi.parser;
+
+/**
+ * Constants for X12 documents.
+ */
+public interface X12Constants
+{
+    // configuration properties
+    public static final String CHAR_SET = "Character set";
+    
+    // interchange properties from ISA segment
+    public static final String AUTHORIZATION_QUALIFIER = "Authorization qualifier";
+    public static final String AUTHORIZATION_INFO = "Authorization info";
+    public static final String SECURITY_QUALIFIER = "Security qualifier";
+    public static final String SECURITY_INFO = "Security info";
+    public static final String SENDER_ID_QUALIFIER = "Sender identification qualifier";
+    public static final String SENDER_ID = "Sender identification";
+    public static final String RECEIVER_ID_QUALIFIER = "Receiver identification qualifier";
+    public static final String RECEIVER_ID = "Receiver identification";
+    public static final String INTERCHANGE_DATE = "Interchange date";
+    public static final String INTERCHANGE_TIME = "Interchange time";
+    public static final String VERSION_ID = "Version identification";
+    public static final String INTER_CONTROL = "Interchange control";
+    public static final String ACK_REQUESTED = "Ack requested";
+    public static final String TEST_INDICATOR = "Test indicator";
+    
+    // group properties from GS segment
+    public static final String FUNCTIONAL_IDENTIFIER_CODE = "Functional identifier code";
+    public static final String APPLICATION_SENDERS_CODE = "Application sender's code";
+    public static final String APPLICATION_RECEIVERS_CODE = "Application receiver's code";
+    public static final String GROUP_DATE = "Group date";
+    public static final String GROUP_TIME = "Group time";
+    public static final String GROUP_CONTROL_NUMBER = "Group control number";
+    public static final String RESPONSIBLE_AGENCY_CODE = "Responsible agency code";
+    public static final String VERSION_IDENTIFIER_CODE = "Version / release/ industry identifier code";
+    
+    // group properties from GE segment
+    public static final String NUMBER_OF_TRANSACTION_SETS = "Number of transaction sets included";
+//    public static final String GROUP_CONTROL_NUMBER = "Group control number";
+    
+    // transaction set properties from ST segment
+    public static final String TRANSACTION_SET_IDENTIFIER_CODE = "Transaction set identifier code";
+    public static final String TRANSACTION_SET_CONTROL_NUMBER = "Transaction set control number";
+    public static final String IMPLEMENTATION_CONVENTION_REFERENCE = "Implementation convention reference";
+    
+    // transaction set properties from SE segment
+    public static final String NUMBER_OF_INCLUDED_SEGMENTS = "Number of included segments";
+//    public static final String TRANSACTION_SET_CONTROL_NUMBER = "Transaction set control number";
+}
