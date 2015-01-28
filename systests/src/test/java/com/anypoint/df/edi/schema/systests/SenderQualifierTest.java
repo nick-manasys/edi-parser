44b6c3c678868f368a367f8a93bc682ecf82f837
pentreathm <matias.pentreath@mulesoft.com>
2015-01-28 15:43:42 -0300

    Adding sender qualifier test

---
+++ systests/src/test/java/com/anypoint/df/edi/schema/systests/SenderQualifierTest.java
@@ -0,0 +1,55 @@
+package com.anypoint.df.edi.schema.systests;
+
+import junit.framework.Assert;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SenderQualifierTest extends X12TestBase {
+
+	private static final String SCHEMA = "/x12/005010/850.esl";
+	
+	private String inputFilePath;
+	private String exceptionText;
+	
+	public SenderQualifierTest(){
+		setInputFilePath("/x12/005010/biztalk-interop/850x1.edi");
+		setExceptionText("Interchange sender infromation does not match configuration");
+	}
+	
+	
+	@BeforeClass
+	public static void setUpClass() {
+		loadSchema(SCHEMA);
+	}
+	
+	@Test
+	public void qualifierMissmatch() throws Exception {
+		String ediParseResult = parsWithSenderIdentityInformation(getInputFilePath(), "AA", "BB", "TEST");
+		Assert.assertTrue(ediParseResult.contains(getExceptionText()));
+	}
+	
+	@Test 
+	public void qualifierMatch() throws Exception {
+		String ediParseResult = parsWithSenderIdentityInformation(getInputFilePath(), "ZZ", "MULESOFT", "");
+		Assert.assertFalse(ediParseResult.contains(getExceptionText()));
+	}
+
+	
+	private String getInputFilePath(){
+		return this.inputFilePath; 
+	}
+	
+	private void setInputFilePath(String filePath){
+		this.inputFilePath = filePath; 
+	}
+	
+	private String getExceptionText(){
+		return this.exceptionText; 
+	}
+	
+	private void setExceptionText(String text){
+		this.exceptionText = text; 
+	}
+}
+
