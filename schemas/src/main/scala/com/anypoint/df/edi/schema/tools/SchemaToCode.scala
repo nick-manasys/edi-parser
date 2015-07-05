bb6cee625fdb0ae8fa45f2547229e19a80206884
Dennis Sosnoski <dms@sosnoski.com>
2015-07-05 16:02:13 +1200

    Code generation improvements to handle HL7 schema definitions.

--- schemas/src/main/scala/com/anypoint/df/edi/schema/tools/SchemaToCode.scala
+++ schemas/src/main/scala/com/anypoint/df/edi/schema/tools/SchemaToCode.scala
@@ -7,7 +7,9 @@ import java.io.File
 import com.anypoint.df.edi.schema.EdiSchema
 import com.anypoint.df.edi.schema.EdiSchema._
 import com.anypoint.df.edi.schema.YamlReader
+
 import scala.annotation.migration
+import scala.annotation.tailrec
 
 /** Print the code to create a segment or transaction. Note that this does not handle variant groups. */
 class SchemaDump(schema: EdiSchema, writer: PrintWriter) {
@@ -55,24 +57,35 @@ class SchemaDump(schema: EdiSchema, writer: PrintWriter) {
   def inlineElement(elem: Element) =
     s"""Element("${elem.ident}", "${elem.name}", ${elem.dataType}, ${elem.minLength}, ${elem.maxLength})"""
 
-  def inlineSegmentComponent(comp: SegmentComponent, compnames: Map[Composite, String], elemrefs: Set[Element]) =
-    comp match {
-      case elem: ElementComponent => {
-        val element = elem.element
+  /** Build inline segment component definition.
+   *  @param scomp
+   *  @param compnames component ident to (single use, component name) map
+   *  @param elemrefs predefined elements
+   */
+  def inlineSegmentComponent(scomp: SegmentComponent, compnames: Map[String, (Boolean, String)], elemrefs: Set[Element]) =
+    scomp match {
+      case ecomp: ElementComponent => {
+        val element = ecomp.element
         val ref = if (elemrefs.contains(element)) genName(element) else inlineElement(element)
-        val name = if (comp.name == element.name) "None" else s"""Some("${comp.name}")"""
-        s"""ElementComponent($ref, $name, "${comp.key}", ${elem.position}, ${elem.usage}, ${elem.count})"""
+        val name = if (scomp.name == element.name) "None" else s"""Some("${scomp.name}")"""
+        s"""ElementComponent($ref, $name, "${scomp.key}", ${ecomp.position}, ${ecomp.usage}, ${ecomp.count})"""
       }
-      case comp: CompositeComponent =>
-        if (comp.count > 1) {
-          s"""CompositeComponent(${compnames(comp.composite)}, Some("${comp.name}"), "${comp.key}", ${comp.position}, ${comp.usage}, ${comp.count})"""
-        } else {
-          s"""CompositeComponent(${compnames(comp.composite)}.rewrite("${comp.key}", convertEdiForm("${schema.ediForm}")), Some("${comp.name}"), "${comp.key}", ${comp.position}, ${comp.usage}, ${comp.count})"""
-        }
+      case ccomp: CompositeComponent => {
+        val ident = ccomp.composite.ident
+        val cname = compnames(ident)._2
+        val comp =
+          if (ccomp.count != 1 || compnames(ident)._1) cname
+          else s"""$cname.rewrite("${ccomp.key}", convertEdiForm("${schema.ediForm}"))"""
+        s"""CompositeComponent($comp, Some("${ccomp.name}"), "${ccomp.key}", ${ccomp.position}, ${ccomp.usage}, ${ccomp.count})"""
+      }
     }
 
-  /** Build segment components code. */
-  def componentList(comps: List[SegmentComponent], compnames: Map[Composite, String], elemrefs: Set[Element]): Unit = {
+  /** Build segment components code.
+   *  @param comps
+   *  @param compnames component ident to (single use, component name) map
+   *  @param elemrefs predefined elements
+   */
+  def componentList(comps: List[SegmentComponent], compnames: Map[String, (Boolean, String)], elemrefs: Set[Element]): Unit = {
     comps.foreach(comp => {
       builder.breakAppend(inlineSegmentComponent(comp, compnames, elemrefs))
       builder.prepend = ", "
@@ -80,22 +93,33 @@ class SchemaDump(schema: EdiSchema, writer: PrintWriter) {
     builder.prepend = ""
   }
 
+  /** Build element definition code. */
   def defineElement(element: Element) = {
     builder.append(s"""val ${genName(element)} = Element("${element.ident}", "${element.name}", ${element.dataType}, ${element.minLength}, ${element.maxLength})""")
     builder.break
   }
 
-  /** Build composite definition code. */
-  def defineComposite(comp: Composite, name: String, compnames: Map[Composite, String], elemrefs: Set[Element]) = {
+  /** Build composite definition code.
+   *  @param comp
+   *  @param name
+   *  @param compnames component ident to (single use, component name) map
+   *  @param elemrefs predefined elements
+   */
+  def defineComposite(comp: Composite, name: String, compnames: Map[String, (Boolean, String)], elemrefs: Set[Element]) = {
     builder.append(s"""val $name = Composite("${comp.ident}", "${comp.name}", List[SegmentComponent](""")
     builder.indent
     componentList(comp.components, compnames, elemrefs)
-    builder.append("), Nil)")
+    builder.append(s"), Nil, ${comp.maxLength})")
     builder.outdent
     builder.break
   }
 
-  def defineSegment(segment: Segment, compnames: Map[Composite, String], elemrefs: Set[Element]) = {
+  /** Build segment definition code.
+   *  @param segment
+   *  @param compnames component ident to (single use, component name) map
+   *  @param elemrefs predefined elements
+   */
+  def defineSegment(segment: Segment, compnames: Map[String, (Boolean, String)], elemrefs: Set[Element]) = {
     builder.append(s"""val ${genName(segment)} = Segment("${segment.ident}", "${segment.name}", List[SegmentComponent](""")
     builder.indent
     componentList(segment.components, compnames, elemrefs)
@@ -132,32 +156,60 @@ class SchemaDump(schema: EdiSchema, writer: PrintWriter) {
     builder.prepend = ""
   }
 
-  def toCode(segments: List[Segment]): Unit = {
-    val elemrefs = segments.foldLeft(Map[Element, Int]())((acc, segment) =>
-      segment.components.foldLeft(acc)((acc, comp) =>
-        comp match {
-          case elem: ElementComponent => acc + (elem.element -> (acc.getOrElse(elem.element, 0) + 1))
-          case _ => acc
-        }))
-    val elemreps = elemrefs.filter(pair => pair._2 > 1).map(pair => pair._1).toSet
-    elemreps.toList.sortBy(element => element.ident).foreach(element => defineElement(element))
-    val composites = segments.foldLeft(Set[Composite]())((acc, segment) =>
-      segment.components.foldLeft(acc)((acc, comp) =>
-        comp match {
-          case ccomp: CompositeComponent => acc + ccomp.composite
-          case _ => acc
-        }))
-    val compnames = composites.toList.sortBy(_.ident).foldLeft(Map[Composite, String]()) {
-      case (map, comp) => {
+  def toCode(segments: List[Segment], composites: Map[String, Composite]): Unit = {
+
+    def elemRefCounts(list: List[SegmentComponent], counts: Map[Element, Int]): Map[Element, Int] =
+      list.foldLeft(counts)((acc, scomp) => scomp match {
+        case ccomp: CompositeComponent => elemRefCounts(ccomp.composite.components, acc)
+        case ecomp: ElementComponent => acc + (ecomp.element -> (acc.getOrElse(ecomp.element, 0) + 1))
+      })
+
+    val elemrefs = segments.foldLeft(Map[Element, Int]())((acc, segment) => elemRefCounts(segment.components, acc))
+    val elemreps = elemrefs.filter(pair => pair._2 > 1).map(_._1).toSet
+    elemreps.toList.sortBy(_.ident).foreach(element => defineElement(element))
+
+    def compRefCounts(list: List[SegmentComponent], counts: Map[String, Int]): Map[String, Int] =
+      list.foldLeft(counts)((acc, scomp) => scomp match {
+        case ccomp: CompositeComponent =>
+          val ident = ccomp.composite.ident
+          if (acc.contains(ident)) acc + (ident -> (acc(ident) + 1))
+          else compRefCounts(ccomp.composite.components, acc + (ident -> 1))
+        case _ => acc
+      })
+      
+    val comprefs = segments.foldLeft(Map[String, Int]())((acc, segment) => compRefCounts(segment.components, acc))
+    val compsings = comprefs.filter(_._2 == 1).map(_._1).toSet
+    
+    def defineComposites(comps: List[Composite], built: Map[String, (Boolean, String)]): Map[String, (Boolean, String)] = {
+      val tobuild = comps.filter { comp =>
+        comp.components.forall {
+          case ccomp: CompositeComponent => built.contains(ccomp.composite.ident)
+          case _ => true
+        }
+      }.sortBy(_.ident)
+      val merged = tobuild.foldLeft(built)((acc, comp) => {
         val name = genName(comp)
-        defineComposite(comp, name, map, elemreps)
-        map + (comp -> name)
-      }
+        val single = compsings.contains(comp.ident)
+        val build = if (single) comp else composites(comp.ident)
+        defineComposite(build, name, acc, elemreps)
+        acc + (comp.ident -> (single, name))
+      })
+      val remain = comps.filter { !tobuild.contains(_) }
+      if (remain.isEmpty) merged
+      else defineComposites(remain, merged)
     }
+
+    val multcomps = comprefs.filter(_._2 != 1).map(pair => composites(pair._1)).toList
+    val allcomps = segments.foldLeft(multcomps)((acc, segment) =>
+      segment.components.foldLeft(acc)((acc, scomp) => scomp match {
+        case ccomp: CompositeComponent if (compsings.contains(ccomp.composite.ident)) => ccomp.composite :: acc
+        case _ => acc
+      }))
+    val compnames = defineComposites(allcomps, Map[String, (Boolean, String)]())
     segments.toList.sortBy(segment => segment.ident).foreach(segment => defineSegment(segment, compnames, elemreps))
   }
 
-  def toCode(trans: Transaction): Unit = {
+  def toCode(trans: Transaction, composites: Map[String, Composite]): Unit = {
     def referencedSegments(comps: List[TransactionComponent]): Set[Segment] = {
       def referencer(comps: List[TransactionComponent], segments: Set[Segment]): Set[Segment] =
         comps.foldLeft(segments)((segs, comp) => comp match {
@@ -175,7 +227,7 @@ class SchemaDump(schema: EdiSchema, writer: PrintWriter) {
       }
     val segments = referencedSegments(trans.heading) ++ referencedSegments(trans.detail) ++
       referencedSegments(trans.summary)
-    toCode(segments.toList)
+    toCode(segments.toList, composites)
     builder.break
     builder.append(s"""val trans${trans.ident} = Transaction("${trans.ident}", "${trans.name}", ${optionText(trans.group)}, List[TransactionComponent](""")
     builder.indent
@@ -198,7 +250,7 @@ object SegmentsToCode {
     val yamlin = new InputStreamReader(new FileInputStream(new File(args(0))), "UTF-8")
     val schema = new YamlReader().loadYaml(yamlin, Array())
     val writer = new PrintWriter(System.out)
-    new SchemaDump(schema, writer).toCode(args.toList.tail.map { ident => schema.segments(ident) })
+    new SchemaDump(schema, writer).toCode(args.toList.tail.map { ident => schema.segments(ident) }, schema.composites)
     writer.flush
     writer.close
   }
@@ -213,7 +265,7 @@ object SchemaToCode {
     val schema = new YamlReader().loadYaml(yamlin, Array())
     val writer = new PrintWriter(System.out)
     val dumper = new SchemaDump(schema, writer)
-    schema.transactions.values.foreach(transact => dumper.toCode(transact))
+    schema.transactions.values.foreach(transact => dumper.toCode(transact, schema.composites))
     writer.flush
     writer.close
   }
