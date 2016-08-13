8a89cc21a2590b20df4faac3b04e0759c3e9195a
Nick <nick@Nicks-MBP.station>
2016-08-14 10:49:58 +1200

    Added code to parse message structures from lines in the file msg_struct_id_segments.txt For each version we parse all schemas of all message types and events
    Added basic unit test for a single message schema

--- schemas/src/main/scala/com/mulesoft/flatfile/schema/convert/HL7TablesConverter.scala
+++ schemas/src/main/scala/com/mulesoft/flatfile/schema/convert/HL7TablesConverter.scala
@@ -8,8 +8,32 @@ import scala.annotation.tailrec
 import scala.collection.{ immutable => sci, mutable => scm }
 import scala.io.Source
 
+import com.mulesoft.flatfile.lexical.HL7Support
+import com.mulesoft.flatfile.schema.EdiSchema
+import com.mulesoft.flatfile.schema.EdiSchema.ComponentBase
+import com.mulesoft.flatfile.schema.EdiSchema.Composite
+import com.mulesoft.flatfile.schema.EdiSchema.CompositeComponent
+import com.mulesoft.flatfile.schema.EdiSchema.DefinedPosition
+import com.mulesoft.flatfile.schema.EdiSchema.Element
+import com.mulesoft.flatfile.schema.EdiSchema.ElementComponent
+import com.mulesoft.flatfile.schema.EdiSchema.GroupComponent
+import com.mulesoft.flatfile.schema.EdiSchema.HL7
+import com.mulesoft.flatfile.schema.EdiSchema.MandatoryUsage
+import com.mulesoft.flatfile.schema.EdiSchema.OptionalUsage
+import com.mulesoft.flatfile.schema.EdiSchema.ReferenceComponent
+import com.mulesoft.flatfile.schema.EdiSchema.Segment
+import com.mulesoft.flatfile.schema.EdiSchema.SegmentComponent
+import com.mulesoft.flatfile.schema.EdiSchema.Structure
+import com.mulesoft.flatfile.schema.EdiSchema.StructureComponent
+import com.mulesoft.flatfile.schema.EdiSchema.StructureSequence
+import com.mulesoft.flatfile.schema.EdiSchema.UnusedUsage
+import com.mulesoft.flatfile.schema.EdiSchema.Usage
+import com.mulesoft.flatfile.schema.EdiSchemaVersion
+import com.mulesoft.flatfile.schema.YamlReader
+import com.mulesoft.flatfile.schema.YamlWriter
+
 /** Application to generate HL7 message schemas from table data.
-  */
+ */
 object HL7TablesConverter {
 
   // YAML file extension
@@ -31,9 +55,9 @@ object HL7TablesConverter {
   /** Specialized usage code conversion for HL7. */
   def convertUsage(code: String) = code match {
     case "B" | "(B) R" | "RE" | "" => OptionalUsage
-    case "W" | "X" => UnusedUsage
-    case "R" => MandatoryUsage
-    case _ => EdiSchema.convertUsage(code)
+    case "W" | "X"                 => UnusedUsage
+    case "R"                       => MandatoryUsage
+    case _                         => EdiSchema.convertUsage(code)
   }
 
   /** Split comma-separated quoted values from string into list. */
@@ -55,8 +79,8 @@ object HL7TablesConverter {
   def fileInput(dir: File, name: String) = new FileInputStream(new File(dir, name))
 
   /** Accumulate the result of applying an operation to the lists of values for each line in input. Note that the result
-    * will be in reversed order (if ordered).
-    */
+   * will be in reversed order (if ordered).
+   */
   def foldInput[T](in: InputStream, z: T)(f: (T, List[String]) => T) =
     Source.fromInputStream(in, "UTF-8").getLines.filter(line => line.length > 0).foldLeft(z)((z, line) =>
       f(z, splitValues(line)))
@@ -69,7 +93,7 @@ object HL7TablesConverter {
   def nameMap(in: InputStream) = foldInput(in, Map[String, Array[String]]())((map, list) =>
     list match {
       case name :: t => map + (name -> t.toArray)
-      case _ => throw new IllegalArgumentException("wrong number of values in file")
+      case _         => throw new IllegalArgumentException("wrong number of values in file")
     })
 
   /** List of pairs, with the first item a key value and the second item a list of lists of associated values. */
@@ -77,19 +101,19 @@ object HL7TablesConverter {
   val emptyListOfKeyedLists = Nil.asInstanceOf[ListOfKeyedLists]
 
   /** Gather list of values into list of lists based on the first column value. This supports the detail file formats
-    * where the first column is a repeating ID value. The output is in the form of a list of pairs, with the first item
-    * the common first column value and the second item a list of the remaining column values. Input order is preserved
-    * in the output, and the column count is verified.
-    *
-    * @param in
-    * @param count
-    * @param fill value used when last column not present (if supplied)
-    * @return List(id, List[List[String]])
-    */
+   * where the first column is a repeating ID value. The output is in the form of a list of pairs, with the first item
+   * the common first column value and the second item a list of the remaining column values. Input order is preserved
+   * in the output, and the column count is verified.
+   *
+   * @param in
+   * @param count
+   * @param fill value used when last column not present (if supplied)
+   * @return List(id, List[List[String]])
+   */
   def gatherGroups(source: String, in: InputStream, count: Int, fill: Option[String]): ListOfKeyedLists = {
     def append(id: String, next: List[String], acc: ListOfKeyedLists) = acc match {
       case (key, values) :: tail if (key == id) => (key, next :: values) :: tail
-      case tail => (id, next :: Nil) :: tail
+      case tail                                 => (id, next :: Nil) :: tail
     }
     val result = foldInput(in, emptyListOfKeyedLists)((acc, list) => {
       val length = list.length
@@ -106,7 +130,7 @@ object HL7TablesConverter {
 
   /** Write schema to file. */
   def writeSchema(schema: EdiSchema, name: String, imports: Array[String], outdir: File) = {
-//    println(s"writing schema $name")
+    //    println(s"writing schema $name")
     val file = new File(outdir, name + yamlExtension)
     val writer = new OutputStreamWriter(new FileOutputStream(file), "UTF-8")
     YamlWriter.write(schema, imports, writer)
@@ -122,9 +146,9 @@ object HL7TablesConverter {
 
   /** Build composite definition from data structure components list. */
   def buildComposite(lines: LineFields, names: Map[String, String], elems: Map[String, Array[String]],
-    comps: Map[String, ComponentBase]) = {
+                     comps: Map[String, ComponentBase]) = {
     val ident = lines.head(0)
-//    println(s"building composite $ident")
+    //    println(s"building composite $ident")
     val compList = lines.map { line =>
       comps(elems(line(2))(2)) match {
         case Element(id, nm, typ) => {
@@ -142,7 +166,7 @@ object HL7TablesConverter {
 
   @tailrec
   def buildComposites(grouped: Map[String, LineFields], names: Map[String, String],
-    compdets: Map[String, Array[String]], built: Map[String, ComponentBase]): Map[String, ComponentBase] = {
+                      compdets: Map[String, Array[String]], built: Map[String, ComponentBase]): Map[String, ComponentBase] = {
 
     val missingDetails = scm.Set[String]()
 
@@ -189,7 +213,7 @@ object HL7TablesConverter {
   }
 
   def buildSegments(grouped: Map[String, LineFields], names: Map[String, String],
-    comps: Map[String, (String, ComponentBase)]) = {
+                    comps: Map[String, (String, ComponentBase)]) = {
     var failed = false
     val result = names.keys.toList.sorted.foldLeft(sci.ListMap[String, Segment]())((map, ident) => {
       val compList = grouped.get(ident) match {
@@ -203,7 +227,7 @@ object HL7TablesConverter {
             val (nmtext, comp) = comps(line(2))
             val name = if (nmtext.isEmpty) None else Some(nmtext)
             comp match {
-              case e: Element => ElementComponent(e, name, "", line(1).toInt, convertUsage(line(3)), repeats) :: acc
+              case e: Element   => ElementComponent(e, name, "", line(1).toInt, convertUsage(line(3)), repeats) :: acc
               case c: Composite => CompositeComponent(c, name, "", line(1).toInt, convertUsage(line(3)), repeats) :: acc
             }
           } catch {
@@ -234,15 +258,15 @@ object HL7TablesConverter {
               val comp = nested.head
               val mrguse = (use, comp.usage) match {
                 case (OptionalUsage, _) | (_, OptionalUsage) => OptionalUsage
-                case _ => use
+                case _                                       => use
               }
               val mrgcnt = (count, comp.count) match {
                 case (0, _) | (_, 0) => 0
-                case _ => count
+                case _               => count
               }
               comp match {
                 case r: ReferenceComponent => new ReferenceComponent(r.segment, r.position, mrguse, mrgcnt)
-                case g: GroupComponent => new GroupComponent(ident, mrguse, 0, g.seq, None, Nil, ch = choice)
+                case g: GroupComponent     => new GroupComponent(ident, mrguse, 0, g.seq, None, Nil, ch = choice)
               }
             } else new GroupComponent(ident, use, count, StructureSequence(true, nested), None, Nil, ch = choice)
           (t, group)
@@ -283,9 +307,9 @@ object HL7TablesConverter {
                 val (rest, group) = buildGroup(h(3), t, "}", MandatoryUsage, 0, true)
                 buildr(rest, group :: comps)
               }
-              case "|" => buildr(t, comps)
+              case "|"                    => buildr(t, comps)
               case "}" | "]" | "}]" | ">" => (remain, comps.reverse)
-              case _ => throw new IllegalArgumentException(s"Unknown segment ${h(2)}")
+              case _                      => throw new IllegalArgumentException(s"Unknown segment ${h(2)}")
             }
           }
         }
@@ -300,10 +324,10 @@ object HL7TablesConverter {
   }
 
   /** Builds schemas from HL7 table data and outputs the schemas in YAML form. The arguments are 1) path to the
-    * directory containing the HL7 table data files, and 2) path to the directory for the YAML output files. All
-    * existing files are deleted from the output directory before writing any output files. Each message is output
-    * as a separate file, with the structure ID used as the file name (with extension ".yaml").
-    */
+   * directory containing the HL7 table data files, and 2) path to the directory for the YAML output files. All
+   * existing files are deleted from the output directory before writing any output files. Each message is output
+   * as a separate file, with the structure ID used as the file name (with extension ".yaml").
+   */
   def main(args: Array[String]): Unit = {
     val hl7dir = new File(args(0))
     val yamldir = new File(args(1))
@@ -315,18 +339,20 @@ object HL7TablesConverter {
     }
     else yamldir.mkdirs
     val yamlrdr = new YamlReader()
-    hl7dir.listFiles.foreach(version => {
+    hl7dir.listFiles.filter { x => x.getAbsoluteFile().getName.contains("2_3_1") }.foreach(version => {
       println(s"Processing ${version.getName}")
       val vernum = version.getName.drop(1).replace('_', '.')
       val schemaVersion = EdiSchemaVersion(HL7, vernum)
 
       // comp_no, description, table_id, data_type_code, data_structure
-      val compDetails = nameMap(fileInput(version, componentDetails))
+      var compDetails = nameMap(fileInput(version, componentDetails))
+      // NJI compDetails = compDetails.+(("9999", Array("9999","varies","0","ST","ST")))
       // data_structure, description, data_type_code, repeating, elementary
       val dataStructs = lineList(fileInput(version, dataStructureNames))
+      dataStructs.++(Array("varies", "var", "varies", "0", "1"))
       val structNames = dataStructs.foldLeft(Map[String, String]())((acc, line) => acc + (line(0) -> line(1)))
       // (data_structure, seq_no), comp_no, table_id, min_length, max_length, req_opt
-      val groupedDataStructs = lineList(fileInput(version, dataStructureComponents)).reverse.groupBy { _(0) }
+      val groupedDataStructs = lineList(fileInput(version, dataStructureComponents)) /* NJI .+:(Array("varies","1","9999","0","","0","O")) */ .reverse.groupBy { _(0) }
 
       // build element definitions for elementary components
       val missingComps = scm.Set[String]()
@@ -348,19 +374,21 @@ object HL7TablesConverter {
       }).sortBy { _.ident }
       val elemDefs = elems.foldLeft(sci.ListMap[String, Element]())((map, elem) => map + (elem.ident -> elem))
       if (missingComps.nonEmpty) println(s"WARNING: Missing components for data structures: $missingComps")
-//      println("Generated element definitions:")
-//      elemDefs.keys.foreach { ident => println(ident) }
-//      val simpleComps = compDetails.filter { case (ident, line) => elemDefs.contains(line(3)) }
+      //      println("Generated element definitions:")
+      //      elemDefs.keys.foreach { ident => println(ident) }
+      //      val simpleComps = compDetails.filter { case (ident, line) => elemDefs.contains(line(3)) }
       val compMap = buildComposites(groupedDataStructs, structNames, compDetails, elemDefs)
-//      compMap.foreach {
-//        case (key, elem: Element) => println(s"$key => ${elem.name} (element)")
-//        case (key, comp: Composite) => println(s"$key => ${comp.name} (composite with ${comp.components.length} components)")
-//      }
+      if (false) {
+        compMap.foreach {
+          case (key, elem: Element)   => println(s"$key => ${elem.name} (element)")
+          case (key, comp: Composite) => println(s"$key => ${comp.name} (composite with ${comp.components.length} components)")
+        }
+      }
 
       // data_item, description, data_structure, min_length, max_length
       val elemMap = lineList(fileInput(version, dataElements)).foldLeft(Map[String, (String, ComponentBase)]())((acc, line) =>
         compMap.get(line(2)) match {
-          case Some(c) => acc + (line(0) -> (line(1), c))
+          case Some(c)                  => acc + (line(0) -> (line(1), c))
           case None if (line(2) == "-") => acc + (line(0) -> (line(1), compMap("varies")))
           case _ =>
             println(s"WARNING: Failed lookup for ident ${line(2)}")
@@ -373,14 +401,17 @@ object HL7TablesConverter {
       val segNames = lineList(fileInput(version, segmentNames)).filter { line => (line(2) == "1") }.
         foldLeft(Map[String, String]()) { (acc, line) => acc + (line(0) -> line(1)) }
       val segments = buildSegments(groupedSegStructs, segNames, elemMap)
-//      println("Segments:")
-//      segments.foreach {
-//        case (key, comp) => println(s" $key => ${comp.name} with ${comp.components.size} top-level components")
-//      }
+      if (false) {
+        println("Segments:")
+        segments.foreach {
+          case (key, comp) =>
+            println(s" $key => ${comp.name} with ${comp.components.size} top-level components")
+        }
+      }
       val compDefs = compMap.keys.toList.sorted.foldLeft(sci.ListMap[String, Composite]()) { (acc, key) =>
         compMap(key) match {
           case c: Composite => acc + (key -> c)
-          case _ => acc
+          case _            => acc
         }
       }
 
@@ -402,6 +433,160 @@ object HL7TablesConverter {
         listWriter.write(struct.ident + '\n')
       })
       listWriter.close
+
+      if (true) {
+        processStructures(groupedMsgStructs)
+      }
     })
   }
+
+  // --------------------------------------------------------
+
+  def processStructures(groupedMsgStructs: Map[String, LineFields]): Unit = {
+    /**
+     *
+     */
+    var structs = new HashMap[String, List[Line]]()
+    var structsMap = new HashMap[String, Message]()
+    for ((messageType, lines) <- groupedMsgStructs) {
+      // print(messageType)
+      //print(lines)
+      // 
+      for (fields <- lines) {
+        // print(fields)
+        val x = Line(fields);
+        // println("    " + x)
+        if (!structs.contains(fields(0)))
+          structs.put(fields(0), List())
+        structs.put(fields(0), structs(fields(0)).++(List(x)))
+      }
+    }
+
+    // for each key (message_type, event_id) get ordered list of lines and construct the structure from them
+    for ((key, value) <- structs) {
+      // println(key)
+      // println(value)
+      val structure = constructStructure(value)
+      structsMap.put(key, structure)
+    }
+
+  }
+  /**
+   * Construct a structure from a list of lines
+   */
+  def constructStructure(lines: List[Line]): Message = {
+    var message = new Message(lines(0).code, MutableList())
+    var line: Line = null
+    var linesRest = lines
+    // println(lines)
+    while (!linesRest.isEmpty) {
+      line = linesRest.head
+      linesRest = linesRest.tail
+      // println(line)
+      if (groupStart(line.segmentName)) {
+        val (l, sG) = constructSegmentGroupStructure(line.group, line.segmentName.contains("{"), line.segmentName.contains("["), linesRest)
+        linesRest = l
+        message.add(sG)
+      } else
+        message.add(new HL7Segment(line.segmentName, "1".equals(line.cardinality), "1".equals(line.optional)))
+    }
+    //  println(message)
+    message
+  }
+
+  def constructSegmentGroupStructure(name: String, cardinality: Boolean, optional: Boolean, lines: List[Line]): (List[Line], HL7SegmentGroup) = {
+    var result = new HL7SegmentGroup(name, cardinality, optional)
+    var line: Line = null
+    var linesRest = lines
+    while (!linesRest.isEmpty) {
+      line = linesRest.head
+      linesRest = linesRest.tail
+      if (groupStart(line.segmentName)) {
+        val (l, sG) = constructSegmentGroupStructure(line.group, line.segmentName.contains("{"), line.segmentName.contains("["), linesRest)
+        linesRest = l
+        result.add(sG)
+      } else if (groupEnd(line.segmentName)) {
+        return (linesRest, result)
+      } else {
+        result.add(new HL7Segment(line.segmentName, "1".equals(line.cardinality), "1".equals(line.optional)))
+      }
+    }
+    (linesRest, result)
+  }
+
+  def groupStart(segmentName: String) = {
+    "[".equals(segmentName) || "{".equals(segmentName) || "[{".equals(segmentName)
+  }
+
+  def groupEnd(segmentName: String) = {
+    "]".equals(segmentName) || "}".equals(segmentName) || "}]".equals(segmentName)
+  }
+}
+
+object Line {
+  def apply(fields: Array[String]): Line = {
+    val code = fields(0)
+    val position = fields(1)
+    val messageType = fields(2)
+    val group = fields(3)
+    val cardinality = fields(4)
+    val optional = fields(5)
+    new Line(code, position, messageType, group, cardinality, optional)
+  }
+}
+
+class Line(val code: String, position: String, val segmentName: String, val group: String, val cardinality: String, val optional: String) {
+  override def toString(): String = "(" + code + ", " + position + ", " + segmentName + ", " + group + ", " + cardinality + ", " + optional + ")";
+}
+
+/**
+ * Base class for HL7 Segment
+ */
+class HL7SegmentBase {
+
+}
+
+class HL7Segment(val name: String, cardinality: Boolean, optional: Boolean) extends HL7SegmentBase {
+  override def toString(): String = {
+    var result = name
+    if (cardinality) result = "{ " + result + " }"
+    if (optional) result = "[ " + result + " ]"
+    result
+  }
+}
+
+class HL7SegmentGroup(val name: String, cardinality: Boolean, optional: Boolean) extends HL7SegmentBase {
+  val segments = new MutableList[HL7SegmentBase]()
+
+  def add(segment: HL7SegmentBase): Unit = {
+    segments += segment
+  }
+
+  override def toString(): String = {
+    var result = name + "\n"
+    for (segment <- segments) {
+      result += "    " + segment.toString() + "\n"
+    }
+    if (cardinality) result = "{ " + result + "    }"
+    if (optional) result = "[ " + result + "    ]"
+    result = result + " " + name + "\n"
+    result
+  }
+}
+
+class Message(val code: String, val segments: MutableList[HL7SegmentBase]) {
+  def add(segment: HL7SegmentBase): Unit = {
+    segments += segment
+  }
+
+  override def toString(): String = {
+    var result = ""
+    result += "Message Structure: " + code + "\n=====>\n"
+    for (segment <- segments) {
+      result += "    " + segment + "\n"
+    }
+    result += "\n<===="
+    result
+  }
+
 }
---
+++ schemas/src/test/scala/com/mulesoft/flatfile/schema/convert/HL7TablesConverterTests.scala
@@ -0,0 +1,30 @@
+package com.mulesoft.flatfile.schema.convert
+
+import org.scalatest.Matchers
+import org.scalatest.FlatSpec
+
+/**
+ * Tests for HL7 table data converter.
+ */
+class HL7TablesConverterTests extends FlatSpec with Matchers {
+  import HL7TablesConverter._
+
+  behavior of "constructStructure"
+
+  val test = List(
+    new Line("RPL_I02", "1", "MSH", "", "0", "0"),
+    new Line("RPL_I02", "2", "MSA", "", "0", "0"),
+    new Line("RPL_I02", "3", "{", "PROVIDER", "0", "0"),
+    new Line("RPL_I02", "4", "PRD", "", "0", "0"),
+    new Line("RPL_I02", "7", "CTD", "", "1", "1"),
+    new Line("RPL_I02", "10", "}", "", "0", "0"),
+    new Line("RPL_I02", "13", "NTE", "", "1", "1"),
+    new Line("RPL_I02", "18", "DSP", "", "1", "1"),
+    new Line("RPL_I02", "22", "DSC", "", "0", "1"))
+
+  it should "parse lines in a list with matching first field" in {
+    var result = constructStructure(test)
+    result.code should be("RPL_I02")
+    result.segments.size should be(6)
+  }
+}
